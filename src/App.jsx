d98bb97e7330031340671e5a724fabe454cf04de
Jun Santos <xjunsantos@gmail.com>
2026-05-05 01:30:54 +0800

    JSX basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 const App = () => {
-    return <h1>Hello World!</h1>
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <p>Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development.</p>
+            <img src="images/nature.jpg" width="400" alt="Nature" />
+            <input type="text" name="name" placeholder="Enter your name" autoComplete="off" />
+        </>
+    )
 }
 
 export default App
