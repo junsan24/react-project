d08cedbeceff283da405358082215578b2ca2b4b
Jun Santos <xjunsantos@gmail.com>
2026-05-07 01:08:43 +0800

    Updating state using value vs function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import Count from "./Count"
+import { useState } from "react"
 
 const App = () => {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount(count+1)
+        setCount(count+1)
+        setCount(count+1)
+    }
+
+    const icrementFunction = () => {
+        setCount(a => a + 1)
+        setCount(b => b + 1)
+        setCount(c => c + 1)
+    }
 
     return (
-        <>
-            <Count />
-            <br /><br /><hr /><br />
-            <Count />
-        </>
+        <div style={{textAlign: 'center', marginTop: '40px'}}>
+            Count: {count}
+            <br />
+            <button onClick={increment}>Increment</button>
+            <br /><br />
+            <button onClick={icrementFunction}>Increment Function</button>
+        </div>
     )
 }
 
