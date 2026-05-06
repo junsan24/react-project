cdceec868bb94d97e0fa715a29dcd706bc333ec5
Jun Santos <xjunsantos@gmail.com>
2026-05-07 00:55:03 +0800

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-const App = () => {
-    
-    const handleClickEvent = (event) => {
-        event.preventDefault()
-        console.log('Link prevented')
-    }
+import Count from "./Count"
 
-    const handleFormSubmit = (event) => {
-        event.preventDefault()
-        console.log('Form submitted')
-        const formData = new FormData(event.currentTarget)
-        const message = formData.get('message')
-        console.log('Message: '+ message)
-    }
+const App = () => {
 
     return (
         <>
-            <a href="https://example.com" onClick={handleClickEvent}>Link</a>
-            <br/>
-            <br/>
-            <form onSubmit={handleFormSubmit} style={{display: 'flex', gap: '10px'}}>
-                <input type="text" name="message" placeholder="Enter your message" style={{padding: '5px'}} />
-                <button type="submit">Send Message</button>
-            </form>
+            <Count />
+            <br /><br /><hr /><br />
+            <Count />
         </>
     )
 }
