3537f8857042de2138a00b00793df5d653a29dcb
junsan <xjunsantos@gmail.com>
2026-05-07 06:42:16 +0800

    Updating objects in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,53 @@
 import { useState } from "react"
+import { produce }  from "immer"
 
 const App = () => {
-    const [count, setCount] = useState(0)
+    const [user, setUser] = useState({
+        name: 'Bob',
+        details: {
+            age: 30,
+            city: 'New York'
+        }
+    })
 
-    const increment = () => {
-        setCount(count+1)
-        setCount(count+1)
-        setCount(count+1)
+    const wrongUpdate = () => {
+        user.name = 'Alice'
+        setUser(user)
     }
 
-    const icrementFunction = () => {
-        setCount(a => a + 1)
-        setCount(b => b + 1)
-        setCount(c => c + 1)
+    const updateUser = () => {
+        setUser({...user, name: 'Alice'})
+    }
+    
+    const updateLocation = () => {
+        setUser({
+            ...user,
+            details:{
+                ...user.details,
+                city: 'Los Angeles'
+            }   
+        })
+    }
+
+    const updateAge = () => {
+        setUser(produce(user, draft => {
+            draft.details.age = 31
+        }))
     }
 
     return (
-        <div style={{textAlign: 'center', marginTop: '40px'}}>
-            Count: {count}
+       <>
+            <button onClick={wrongUpdate} type="button">Wrong to Update</button>
             <br />
-            <button onClick={increment}>Increment</button>
-            <br /><br />
-            <button onClick={icrementFunction}>Increment Function</button>
-        </div>
+            <button onClick={updateUser} type="button">Update User</button>
+            <br />
+            <button onClick={updateLocation}>Update Location</button>
+            <br />
+            <button onClick={updateAge}>Update Age with Immer</button>
+            <p>Name: {user.name}</p>
+            <p>Age: {user.details.age}</p>
+            <p>City: {user.details.city}</p>
+       </>
     )
 }
 
