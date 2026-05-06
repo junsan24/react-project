200087f478d6a04a578cb59eb3b321bce377a1b5
Jun Santos <xjunsantos@gmail.com>
2026-05-06 22:50:56 +0800

    Event Propagation

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
-import Button from './components/Button'
-
 const App = () => {
     
-    const greetings = () => console.log('Hello World!')
+    const onClickParent = () => console.log('Parent clicked.')
 
-    const changeToGreen = () => {
-        document.body.style.backgroundColor = "lightgreen"
-    }
+    const onClickChild = () => console.log('Child clicked.')
 
-    const changeToBlue = () => {
-        document.body.style.backgroundColor = "lightblue"
+    const onClickSecondChild = (e) => {
+        e.stopPropagation()
+        console.log('Second Child clicked')
     }
 
     return (
         <>
-            <Button onButtonClick={greetings} >
-                Greetings!
-            </Button>
-            <br/><br/>
-            <Button onButtonClick={changeToGreen}>
-                Change to Green
-            </Button>
-            <br/><br/>
-            <Button onButtonClick={changeToBlue}>
-                Change to Blue
-            </Button>
+            <div onClick={onClickParent} style={{
+                padding: '40px',
+                textAlign: 'center',
+                backgroundColor: 'lightgreen'
+            }}>
+                Parent
+                <div onClick={onClickChild} style={{
+                    padding: '40px',
+                    marginTop: '20px',
+                    textAlign: 'center',
+                    backgroundColor: 'lightblue'
+                }}>
+                    Child
+                </div>
+                <div onClick={onClickSecondChild} style={{
+                    padding: '40px',
+                    marginTop: '20px',
+                    textAlign: 'center',
+                    backgroundColor: 'lightblue'
+                }}>
+                    Second Child
+                </div>
+            </div>
         </>
     )
 }
